63f8d1ad8778d5f8b73d2ca6be3e6b894da94562
Firas Abbas <fabbas@rismedia.com>
2020-06-07 14:00:13 -0400

    seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,15 +12,6 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        $user = new User();
-        $user->first_name = 'Phil';
-        $user->last_name = 'Anselmo';
-        $user->email = 'example@example.com';
-        $user->password = bcrypt('123123qweqwe');
-        $user->username = 'freezabb';
-        $user->bio = 'A musician, guitarist and app developer.';
-        $user->save();
 
-        $user->assignRole('admin', 'moderator', 'user');
     }
 }
