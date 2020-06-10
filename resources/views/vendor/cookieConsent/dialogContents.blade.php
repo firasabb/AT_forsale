771283d301dd398309796756a161f2191159ab26
Firas Abbas <fabbas@rismedia.com>
2020-06-10 12:02:17 -0400

    cookie consent no gutters

--- resources/views/vendor/cookieConsent/dialogContents.blade.php
+++ resources/views/vendor/cookieConsent/dialogContents.blade.php
@@ -1,12 +1,12 @@
 <div class="js-cookie-consent cookie-consent" style="text-align: center; padding: 20px 0; background-color: #2b2b2b; color: #fff">
-    <div class="row">
+    <div class="row no-gutters">
         <div class="col">
             <span class="cookie-consent__message">
                 {!! trans('cookieConsent::texts.message') !!}
             </span>
         </div>
     </div>
-    <div class="row mt-2">
+    <div class="row mt-2 no-gutters">
         <div class="col">
             <button class="js-cookie-consent-agree cookie-consent__agree btn btn-sm btn-light mx-4">
                 {{ trans('cookieConsent::texts.agree') }}
