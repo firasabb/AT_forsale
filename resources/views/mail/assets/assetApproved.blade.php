d5d5e725b8e82d08bab3cdb42f2169b72ddf1d29
Firas Abbas <fabbas@rismedia.com>
2020-06-10 16:42:32 -0400

    approved asset

--- resources/views/mail/assets/assetApproved.blade.php
+++ resources/views/mail/assets/assetApproved.blade.php
@@ -1,13 +1,7 @@
 @component('mail::message')
-# Introduction
 
 Congratulations! Your asset is approved!<br>
 Thank you for being a part of us.
-
-@component('mail::button', ['url' => $url, 'color' => 'blue'])
-View My Asset
-@endcomponent
-
-Thanks,<br>
+<br>
 {{ config('app.name') }}
 @endcomponent
--- resources/views/mail/assets/assetRejected.blade.php
+++ resources/views/mail/assets/assetRejected.blade.php
@@ -1,9 +1,7 @@
 @component('mail::message')
-# Introduction
 
 We apologize to inform you that your asset has not been approved, since it doesn't meet our rules and requirements.<br>
 Thank you for being a part of us, and we hope to hear from you again.
-
-Thanks,<br>
+<br>
 {{ config('app.name') }}
 @endcomponent
